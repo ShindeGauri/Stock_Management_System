fe812ee5ef882206a101eb3e5b3da1531439ecc7
Admin <Admin@DESKTOP-6TMS950>
2026-02-17 20:10:20 +0530

    product class created and addEmployee DAO method done!

--- StockManagementSystem/src/dao/ProductDAO.java
+++ StockManagementSystem/src/dao/ProductDAO.java
@@ -5,10 +5,35 @@
  */
 package dao;
 
+import db.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import model.Product;
+
 /**
  *
  * @author Admin
  */
 public class ProductDAO {
-    
+    public boolean addProduct(Product product){
+        Connection conn=DBConnection.getConnection();
+        String sql="INSERT INTO products(id, name, category, price, quantity, supplier) VALUES (?, ?, ?, ?, ?, ?);";
+        try{
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setInt(1,product.getId());
+            pst.setString(2,product.getName());
+            pst.setString(3,product.getCategory());
+            pst.setDouble(4,product.getPrice());
+            pst.setInt(5,product.getQuantity());
+            pst.setString(6,product.getSupplier());
+            pst.executeUpdate();
+            return true;  
+        }
+        catch(SQLException e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            return false;
+        }
+    }   
 }
--- StockManagementSystem/src/model/Product.java
+++ StockManagementSystem/src/model/Product.java
@@ -10,5 +10,37 @@ package model;
  * @author Admin
  */
 public class Product {
+    private int id;
+    private String name;
+    private String category;
+    private double price;
+    private int quantity;
+    private String supplier;
     
+    public Product(int id,String name,String category,double price,int quantity,String supplier){
+        this.id=id;
+        this.name=name;
+        this.category=category;
+        this.price=price;
+        this.quantity=quantity;
+        this.supplier=supplier;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+    public String getSupplier(){
+        return supplier;
+    } 
 }
